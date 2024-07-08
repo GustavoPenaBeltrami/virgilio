80415e0bc27c53a4bb16a5ae7ad616230b906dfc
Gustavo Peña Beltrami <gustavopb99c@gmail.com>
2024-07-08 16:17:04 -0300

    responsive

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { ChatBot } from "@/components/ChatBot";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24 lg:px-24 md:px-20 px-16<">
-      <div className="bg-[#f5f5f5] text-[#584de6] p-20 rounded-xl shadow-xl">
+      <div className="lg:bg-[#f5f5f5] lg:text-[#584de6] bg-transparent text-white p-20 rounded-xl shadow-xl">
         <h2 className="lg:text-3xl text-2xl mb-6 font-bold text-center">
           Bienvenido al tester del chatbot de{" "}
         </h2>
